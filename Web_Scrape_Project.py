e82c124065dfa4581527e041a44af3b91a31d9a5
Tirzah Rossi <tirzahrossi42@gmail.com>
2019-10-24 14:30:41 -0700

    first commit

---
+++ Web_Scrape_Project.py
@@ -0,0 +1,18 @@
+from gazpacho import get, Soup
+import pandas as pd
+
+from urllib.request import urlopen
+
+url = 'http://registration.baa.org/2019/cf/Public/iframe_TopFinishers.htm'
+html = urlopen(url)
+response = get(url)
+soup = Soup(response)
+df = pd.read_html(str(soup.find('table')))
+print(df)
+
+import re
+
+list_rows = []
+
+df = pd.DataFrame(list_rows)
+df.head(10)
